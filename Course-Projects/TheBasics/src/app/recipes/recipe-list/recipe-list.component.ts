29ae0f2d6cb1edb12f0687452002ebf7f4d33e36
ngiakhanh <gnguyen1@mantu.com>
2020-01-16 11:32:32 +0700

    Lesson 53

--- Course-Projects/TheBasics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Course-Projects/TheBasics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('WhiteK Recipe', 'Master Recipe', "https://images-na.ssl-images-amazon.com/images/I/718ebRhi9AL.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
