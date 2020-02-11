5b4b290741dfad0c7cceeef313142863701b5205
ngiakhanh <gnguyen1@mantu.com>
2020-02-11 13:18:22 +0700

    Lesson 113

---
+++ Lessons/Section 9/services-start/src/app/accounts.service.ts
@@ -0,0 +1,32 @@
+import { LoggingService } from "./logging.service";
+import { Injectable, EventEmitter } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+  statusUpdated = new EventEmitter<string>();
+
+  constructor(private loggingService: LoggingService){}
+
+  addAccount(account: {name: string, status: string}){
+    this.accounts.push(account);
+    this.loggingService.logStatusChange(account.status);
+  }
+  updateStatus(updateInfo: {id: number, newStatus: string}){
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+    this.loggingService.logStatusChange(updateInfo.newStatus);
+  }
+}
